7606363851f7801cabc79a0ecca761d49ed6702f
Daniey <lixin520gj@163.com>
2021-02-25 18:39:29 +0800

    修改主页样式

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -4,17 +4,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Translate } from 'react-jhipster';
 import { connect } from 'react-redux';
-import { Row, Col, Alert } from 'reactstrap';
+import { Row, Col, Alert, Button } from 'reactstrap';
 
 import { IRootState } from 'app/shared/reducers';
 
 export type IHomeProp = StateProps;
-
-export const BrandIcon = props => (
-  <div {...props} className="brand-icon">
-    <img src="content/images/logo-jhipster.svg" alt="Logo" />
-  </div>
-);
 
 export const Home = (props: IHomeProp) => {
   const { account } = props;
@@ -23,19 +17,18 @@ export const Home = (props: IHomeProp) => {
     <Row>
       <Col md="12">
         <div className="home-top-01">
-          <BrandIcon />
-          <h2>
-            <Translate contentKey="home.title">Welcome, Java Hipster!</Translate>
-          </h2>
-          <p className="lead">
-            <Translate contentKey="home.subtitle">This is your homepage</Translate>
-          </p>
+          <img src="content/images/yuoai.png" alt="Logo" />
+          <div className="home-top-02">
+            <h3>
+              <Translate contentKey="home.title">Welcome, Java Hipster!</Translate>
+            </h3>
+            <p>
+              <Translate contentKey="home.subtitle">This is your homepage</Translate>
+            </p>
+          </div>
         </div>
-
-
-
         {account && account.login ? (
-          <div>
+          <div className="home-logged-message-div">
             <Alert color="success">
               <Translate contentKey="home.logged.message" interpolate={{ username: account.login }}>
                 You are logged in as user {account.login}.
@@ -43,24 +36,23 @@ export const Home = (props: IHomeProp) => {
             </Alert>
           </div>
         ) : (
-          <div>
-            <Alert color="warning">
-              <Translate contentKey="global.messages.info.authenticated.prefix">If you want to </Translate>
+          <Alert className="home-login-div" color="warning">
+            <span className="home-login-div-span radius-left">
               <Link to="/login" className="alert-link">
                 <Translate contentKey="global.messages.info.authenticated.link"> sign in</Translate>
               </Link>
-            </Alert>
-
-            <Alert color="warning">
-              <Translate contentKey="global.messages.info.register.noaccount">You do not have an account yet?</Translate>&nbsp;
+            </span>
+            <span className="home-login-div-span radius-right">
               <Link to="/account/register" className="alert-link">
                 <Translate contentKey="global.messages.info.register.link">Register a new account</Translate>
               </Link>
-            </Alert>
-          </div>
+            </span>
+          </Alert>
         )}
-        <p></p>
-
+        <div className="home-login-low">
+          <div className="home-login-low-text">本项目使用Jhipster框架开发</div>
+          <div className="home-login-low-text-github">项目稍后会在github开源</div>
+        </div>
       </Col>
     </Row>
   );
